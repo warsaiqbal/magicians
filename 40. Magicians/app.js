070793e39ca1c1be8c5c2c83e7b4b304e7f17242
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-06-05 13:38:45 -0700

    Add files via upload

---
+++ 40. Magicians/app.js
@@ -0,0 +1,7 @@
+var magicians = ["Magician 1", "Magician 2", "Magician 3"];
+function show_magicians(magicians) {
+    magicians.forEach(function (magician) {
+        console.log(magician);
+    });
+}
+show_magicians(magicians);
---
+++ 40. Magicians/app.ts
@@ -0,0 +1,10 @@
+let magicians: string[] = ["Magician 1", "Magician 2", "Magician 3"]
+
+function show_magicians(magicians: string[]){
+    magicians.forEach(magician => {
+        console.log(magician);
+    });
+}
+
+show_magicians(magicians);
+
